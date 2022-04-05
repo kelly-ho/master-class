887a755790206270a7886d76e7af6ac2c86c956d
Kelly Ho <kellyho7@mit.edu>
2022-04-04 23:37:20 -0400

    added more speech commands and changed leap

--- website/app/main.js
+++ website/app/main.js
@@ -33,13 +33,24 @@ var processSpeech = function(transcript) {
   }
   else if (userSaid(transcript, ['masterclass volume up', 'masterclass louder'])) {
     var volume = player.getVolume() + 10;
-    player.setVolume(volume.clamp(0,100)); 
+    player.setVolume(Math.max(Math.min(volume, 100), 0));
   }
   else if (userSaid(transcript, ['masterclass volume down', 'masterclass quieter'])) {
     var volume = player.getVolume() - 10;
-    player.setVolume(volume.clamp(0,100)); 
+    player.setVolume(Math.max(Math.min(volume, 100), 0));
   }
-  ;
+  else if (userSaid(transcript, ['masterclass mute', 'masterclass volume off'])) {
+    player.mute();
+  }
+  else if (userSaid(transcript, ['masterclass unmute', 'masterclass volume on'])) {
+    player.unMute();
+  }
+  else if (userSaid(transcript, ['masterclass speed up', 'masterclass increase speed'])) {
+    increaseSpeed();
+  }
+  else if (userSaid(transcript, ['masterclass slow down', 'masterclass decrease speed'])) {
+    decreaseSpeed()
+  }
 };
 
 // From tutorial https://developers.google.com/youtube/iframe_api_reference#Getting_Started
@@ -77,7 +88,9 @@ function onPlayerReady(event) {
 //    the player should play for six seconds and then stop.
 var done = false;
 function onPlayerStateChange(event) {
-  player.getVideoUrl()
+  console.log("HELLOOOOO");
+  console.log(document.getElementById("title"));
+  document.getElementById("title").focus();
   // if (event.data == YT.PlayerState.PLAYING && !done) {
   //   setTimeout(stopVideo, 1000);
   //   console.log("STEP 5");
@@ -109,51 +122,71 @@ let buffer = 0;
 
 Leap.loop(controllerOptions, function(frame) {
   buffer += 1; //buffer so multiple gestures aren't recognized at once
-  if(frame.gestures.length > 0 && buffer > 50){
-    console.log("gesture");
-    var gesture = frame.gestures[0];
-    switch (gesture.type) {
-      case "circle":
-        console.log("Circle Gesture");
-        var clockwise = false;
-        var pointableID = gesture.pointableIds[0];
-        var direction = frame.pointable(pointableID).direction;
-        if (!(pointableID && direction)) break;
-        var dotProduct = Leap.vec3.dot(direction, gesture.normal);
-        var clockwise = (dotProduct > 0);
-        if (clockwise) {
-          skip(10);
-          console.log("clockwise");
-        } else {
-          skip(-10);
-        }
-        buffer = 0;
+  console.log(buffer);
+  if(frame.gestures.length > 0 && buffer > 200){
+    console.log("gesture", frame.gestures.length);
+    for(let i=0; frame.gestures.length; i++) {
+      if (buffer === 0) { //gesture completed
         break;
-      case "keyTap":
-      case "screenTap":
-        console.log("Tap Gesture");
-        if (player.getPlayerState() == -1 || player.getPlayerState() == 2) {
-          player.playVideo();
-        } else if (player.getPlayerState() == 1) {
-          player.pauseVideo();
-        }
-        buffer = 0;
-        break;
-      case "swipe":
-        console.log("Swipe Gesture");
-        var xAxis = Math.abs(gesture.direction[0]) > Math.abs(gesture.direction[1]);
-        if (xAxis) {
-          if (gesture.direction[0] > 0) { //swipe right
-            player.setPlaybackRate(player.getPlaybackRate() + 0.25);
+      }
+      var gesture = frame.gestures[i];
+      if (!gesture) {
+        continue;
+      }
+      switch (gesture.type) {
+        case "circle":
+          console.log("Circle Gesture");
+          var pointableID = gesture.pointableIds[0];
+          var direction = frame.pointable(pointableID).direction;
+          if (!(pointableID && direction)) break;
+          var dotProduct = Leap.vec3.dot(direction, gesture.normal);
+          var clockwise = (dotProduct > 0);
+          if (clockwise) {
+            skip(10);
+            console.log("clockwise");
           } else {
-            player.setPlaybackRate(player.getPlaybackRate() - 0.25);
+            skip(-10);
           }
-        }
-        buffer = 0;
-        break;
+          buffer = 0;
+          break;
+        case "keyTap":
+        case "screenTap":
+          console.log("Tap Gesture");
+          if (player.getPlayerState() == -1 || player.getPlayerState() == 2) {
+            player.playVideo();
+          } else if (player.getPlayerState() == 1) {
+            player.pauseVideo();
+          }
+          buffer = 0;
+          break;
+        case "swipe":
+          console.log("Swipe Gesture");
+          var xAxis = Math.abs(gesture.direction[0]) > Math.abs(gesture.direction[1]);
+          if (xAxis) {
+            console.log("swipe along x axis");
+            if (gesture.direction[0] > 0) { //swipe right
+              increaseSpeed();
+            } else {
+              decreaseSpeed();
+            }
+            buffer = 0;
+          }
+          break;
+        default:
+          break;
+      }
     }
+
   }
 });
+
+function increaseSpeed() {
+  player.setPlaybackRate(player.getPlaybackRate() + 0.25);
+}
+function decreaseSpeed() {
+  player.setPlaybackRate(player.getPlaybackRate() - 0.25);
+}
+
 
 
 /***
@@ -166,3 +199,22 @@ function skip(seconds){
     player.playVideo();
   }
 };
+
+// function resetInterval() {
+//   clearInterval(monitor);
+//   var monitor = setInterval(clickHandler, 100);
+// };
+//
+//
+// function clickHandler() {
+//   var elem = document.activeElement;
+//   if (elem && elem.tagName == 'IFRAME') {
+//
+//     // alert('clicked!');
+//     console.log('click');
+//     clearInterval(monitor);
+//     // setInterval(clickHandler, 1000);
+//   }
+// };
+//
+// resetInterval()
